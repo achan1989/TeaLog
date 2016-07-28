2a8ae8d16d8a8d40017adff762e7a06a5efe16f4
achan1989 <achan1989@gmail.com>
2016-07-28 23:30:09 +0100

    Initial project structure and minimal tray icon.

---
+++ TeaLog/TeaAppContext.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Drawing;
+
+
+/* 
+ * Copyright (c) 2010 Michael Sorens http://www.simple-talk.com/author/michael-sorens/
+ * Copyright (c) 2016 Adrian Chan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+namespace TeaLog
+{
+    /// <summary>
+    /// Framework for running application as a tray app.
+    /// </summary>
+    /// <remarks>
+    /// Largely copied from a useful guide by Michael Sorens, https://www.simple-talk.com/dotnet/.net-framework/creating-tray-applications-in-.net-a-practical-guide/
+    /// Itself partially adapted from "Creating Applications with NotifyIcon in Windows Forms", Jessica Fosler,
+    /// http://windowsclient.net/articles/notifyiconapplications.aspx
+    /// </remarks>
+    public class TeaAppContext : ApplicationContext
+    {
+        private static readonly string IconFileName = "tea.ico";
+        private static readonly string DefaultTooltip = "Log your tea!";
+        private readonly LogManager logManager;
+
+        /// <summary>
+		/// This class should be created and passed into Application.Run( ... )
+		/// </summary>
+		public TeaAppContext()
+        {
+            InitializeContext();
+
+            logManager = new LogManager(notifyIcon);
+
+            /* TODO: init logic.
+            hostManager = new HostManager(notifyIcon);
+            hostManager.BuildServerAssociations();
+            if (!hostManager.IsDecorated) { ShowIntroForm(); }
+            */
+        }
+
+        private void ContextMenuStrip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            var exitItem = new ToolStripMenuItem("&Exit");
+            exitItem.Click += exitItem_Click;
+            logManager.BuildContextMenu(notifyIcon.ContextMenuStrip, exitItem);
+
+            /* TODO: logic populates menu.
+            hostManager.BuildServerAssociations();
+            hostManager.BuildContextMenu(notifyIcon.ContextMenuStrip);
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+            notifyIcon.ContextMenuStrip.Items.Add(hostManager.ToolStripMenuItemWithHandler("Show &Details", showDetailsItem_Click));
+            notifyIcon.ContextMenuStrip.Items.Add(hostManager.ToolStripMenuItemWithHandler("&Help/About", showHelpItem_Click));
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+            notifyIcon.ContextMenuStrip.Items.Add(hostManager.ToolStripMenuItemWithHandler("&Exit", exitItem_Click));
+            */
+        }
+
+        
+        # region generic code framework
+
+        private System.ComponentModel.IContainer components;	// a list of components to dispose when the context is disposed
+        private NotifyIcon notifyIcon;				            // the icon that sits in the system tray
+
+        private void InitializeContext()
+        {
+            components = new System.ComponentModel.Container();
+            notifyIcon = new NotifyIcon(components)
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = new Icon(IconFileName),
+                Text = DefaultTooltip,
+                Visible = true
+            };
+            notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+            /* notifyIcon.DoubleClick += notifyIcon_DoubleClick; show intro form */
+            notifyIcon.MouseUp += notifyIcon_MouseUp;
+        }
+
+        // From http://stackoverflow.com/questions/2208690/invoke-notifyicons-context-menu
+        private void notifyIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+                mi.Invoke(notifyIcon, null);
+            }
+        }
+
+        /// <summary>
+		/// When the application context is disposed, dispose things like the notify icon.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null) { components.Dispose(); }
+        }
+
+        /// <summary>
+        /// When the exit menu item is clicked, make a call to terminate the ApplicationContext.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            ExitThread();
+        }
+
+        /// <summary>
+        /// If we are presently showing a form, clean it up.
+        /// </summary>
+        protected override void ExitThreadCore()
+        {
+            /* TODO: before we exit, let forms clean themselves up.
+            if (introForm != null) { introForm.Close(); }
+            if (detailsForm != null) { detailsForm.Close(); }
+            */
+
+            notifyIcon.Visible = false; // should remove lingering tray icon
+            base.ExitThreadCore();
+        }
+
+        # endregion generic code framework
+
+    }
+}
