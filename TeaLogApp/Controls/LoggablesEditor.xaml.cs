557e34b91af9f5ba0df9928c196611b647368105
achan1989 <achan1989@gmail.com>
2016-08-06 11:32:54 +0100

    Move the Loggables editor out of the main AppSettingsWindow and into its own custom control.

---
+++ TeaLogApp/Controls/LoggablesEditor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeaLog.Controls
+{
+    /// <summary>
+    /// Interaction logic for LoggablesEditor.xaml
+    /// </summary>
+    public partial class LoggablesEditor : UserControl
+    {
+        public LoggablesEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
